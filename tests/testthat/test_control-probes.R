c1d1746f7f1b9d3352817862ec6078a9f3e49f17
Mark J Chen <mjchen.gene@gmail.com>
2018-11-09 15:36:14 -0600

    refactor: add skip toggle to several tests.

--- tests/testthat/test_control-probes.R
+++ tests/testthat/test_control-probes.R
@@ -2,12 +2,15 @@ context("Quality control in terms of control probes")
 library(yamat)
 library(minfiData)
 
+skip_flag <- TRUE
 
 test_that("control_probe_intensities()", {
+  testthat::skip_if(skip_flag, "Skip")
   testthat::expect_error(df <- control_probe_intensities(RGsetEx), NA)
 })
 
 test_that("plot_control_probes()", {
+  testthat::skip_if(skip_flag, "Skip")
   testthat::expect_error(plots <- plot_control_probes(RGsetEx, s = 1), NA)
 })
 
--- tests/testthat/test_qc.R
+++ tests/testthat/test_qc.R
@@ -2,8 +2,10 @@ context("Quality control wrapper")
 library(yamat)
 library(minfiData)
 
+skip_flag <- TRUE
 report_dir <- tempdir()
 
 test_that("qc()", {
+  testthat::skip_if(skip_flag, "Skip")
   testthat::expect_error(qc(rgset = RGsetEx, report_dir = report_dir), NA)
 })
