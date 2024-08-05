29b1d68b47d8daa6e56fc32d0313856a924bb214
Mark J Chen <mjchen.gene@gmail.com>
2024-08-05 11:20:03 -0400

    fix: update test

--- tests/testthat/test_qc-control-probes_epic.R
+++ tests/testthat/test_qc-control-probes_epic.R
@@ -19,7 +19,7 @@ test_that("sample number", {
 test_that("metric number", {
   testthat::skip_if(skip_flag, "Skip")
   control_probe_qc_epic <- get_test_control_probe_qc_epic()
-  testthat::expect_equal(ncol(control_probe_qc_epic), 61)
+  testthat::expect_equal(ncol(control_probe_qc_epic), 52)
 })
 
 test_that("restoration green intensity has the correct value", {
