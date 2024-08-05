abecacc771d525152abe1cea3e9adb72aa3887cb
Mark J Chen <mjchen.gene@gmail.com>
2024-08-05 08:27:05 -0400

    refact: get test data

--- tests/testthat/test_qc-control-probes.R
+++ tests/testthat/test_qc-control-probes.R
@@ -3,9 +3,21 @@ library(minfiDataEPIC)
 
 skip_flag <- FALSE
 
-test_that("get_control_probe_qc_metrics", {
+get_test_qc <- function(env = parent.frame()) {
+  message(paste("Start", Sys.time()))
+  qc <- get_control_probe_qc_metrics(RGsetEPIC)
+  message(paste("end", Sys.time()))
+  qc
+}
+
+test_that("sample number", {
+  testthat::skip_if(skip_flag, "Skip")
+  qc <- get_test_qc()
+  testthat::expect_equal(nrow(qc), 3)
+})
+
+test_that("metric number", {
   testthat::skip_if(skip_flag, "Skip")
   qc <- get_control_probe_qc_metrics(RGsetEPIC)
-  testthat::expect_equal(nrow(qc), 3)
   testthat::expect_equal(ncol(qc), 60)
 })
