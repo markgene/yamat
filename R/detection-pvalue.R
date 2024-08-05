01a7ad0524566e5945b53347dac659a37b38de18
Mark J Chen <mjchen.gene@gmail.com>
2024-08-05 10:56:53 -0400

    fix: add internal sample ID for joining by sample

--- R/detection-pvalue.R
+++ R/detection-pvalue.R
@@ -7,7 +7,9 @@
 #' @return A \code{data.frame} of summarzing detection p-value for QC purpose.
 #' @export
 get_detection_p_value_qc_metrics <- function(rgset) {
-  pheno_df <- minfi::pData(rgset)
+  pheno_df <- minfi::pData(rgset) %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column(var="InternalSampleId")
   detection_p_values <- minfi::detectionP(rgset)
   probe_total <- colSums(!is.na(detection_p_values))
   probe_le_0_01 <- colSums(detection_p_values <= 0.01)
@@ -16,7 +18,7 @@ get_detection_p_value_qc_metrics <- function(rgset) {
   probe_le_0_05_pct <- probe_le_0_05 / probe_total
   mean_detection_p_values <- colMeans(detection_p_values)
   res <- data.frame(
-    Basename = pheno_df$Basename,
+    InternalSampleId = pheno_df$InternalSampleId,
     Mean_Detection_P_Value = mean_detection_p_values,
     Probe_Detection_P_Value_Less_than_or_Equal_01_Percent = probe_le_0_01_pct,
     Probe_Detection_P_Value_Less_than_or_Equal_05_Percent = probe_le_0_05_pct,
