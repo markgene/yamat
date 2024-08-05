93889381294def1ccdf872d505ffcbb4f4c82337
Mark J Chen <mjchen.gene@gmail.com>
2024-08-05 11:56:34 -0400

    feat: Test get_qc_metrics() with EPIC test dataset

--- tests/testthat/test_get_qc_metrics_epic.R
+++ tests/testthat/test_get_qc_metrics_epic.R
@@ -1,24 +1,21 @@
-context("Test get_qc_metrics() with EPIC")
+context("Test get_qc_metrics() with EPIC test dataset")
 library(yamat)
+library(minfi)
 library(minfiDataEPIC)
 
 skip_flag <- FALSE
 
 get_test_qc_metrics_epic <- function(env = parent.frame()) {
   message(paste("Start", Sys.time()))
-  qc <- get_qc_metrics(RGsetEPIC)
+  rgset <- get_qc_metrics(RGsetEPIC)
   message(paste("end", Sys.time()))
-  qc
+  minfi::pData(rgset) %>%
+    as.data.frame()
 }
 
-test_that("probe x channel number", {
+test_that("Restoration_Green_Intensity has the correct values", {
   testthat::skip_if(skip_flag, "Skip")
   qc_epic <- get_test_qc_metrics_epic()
-  testthat::expect_equal(nrow(ctrl_probe_epic), 1270 * ncol(RGsetEPIC))
-})
-
-test_that("column number", {
-  testthat::skip_if(skip_flag, "Skip")
-  qc_epic <- get_test_qc_metrics_epic()
-  testthat::expect_equal(ncol(ctrl_probe_epic), 10)
+  is_eq <- qc_epic$Restoration_Green_Intensity == c(243, 374, 430)
+  testthat::expect_true(all(is_eq == TRUE))
 })
